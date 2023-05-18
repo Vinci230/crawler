b83b21ccc4bab297c0a3bbf63003122de8673067
estarhp <112890331+estarhp@users.noreply.github.com>
2023-05-18 14:40:45 +0800

    tang

--- wangyi_content/wy.js
+++ wangyi_content/wy.js
@@ -39,18 +39,36 @@ function d(d, e, f, g) {
 
 
 let i2x = {
-    "rid": "R_SO_4_487434788",
-    "threadId": "R_SO_4_487434788",
-    "pageNo": "2",
+    "rid": "R_SO_4_1876909269",
+    "threadId": "R_SO_4_1876909269",
+    "pageNo": "3",
     "pageSize": "20",
-    "cursor": "1668870713082",
+    "cursor": "1683020186913",
     "offset": "0",
     "orderType": "1",
-    "csrf_token": ""
+    "csrf_token": "154c4129b78f478464f1a79022ab11d5"
 }
+
 let e='00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7'
 let g='0CoJUm6Qyw8W8jud'
-console.log(d(JSON.stringify(i2x),'010001',e,g))
+
+function p(gG7z, UP8H, cKV9M) {
+        if (!gG7z)
+            return "";
+        var bx5C = [];
+        for (var x in gG7z) {
+            bx5C.push(encodeURIComponent(x) + "=" + (!!cKV9M ? encodeURIComponent(gG7z[x]) : gG7z[x]))
+        }
+        return bx5C.join(UP8H || ",")
+    }
+
+    bMs6m = d(JSON.stringify(i2x),'010001',e,g)
+
+function main(){
+    return p({ params: bMs6m.encText,
+                encSecKey: bMs6m.encSecKey},"&",!0)
+}
+
 //hhhjjjhhh
 //找到加密参数的过程
 //1.找到所需例如评论的所在网址
