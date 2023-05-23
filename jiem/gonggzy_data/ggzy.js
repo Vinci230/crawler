5f09b489d265e5520a503bb71b95b100bcde34ec
estarhp <112890331+estarhp@users.noreply.github.com>
2023-05-23 19:19:34 +0800

    fj

--- jiem/gonggzy_data/ggzy.js
+++ jiem/gonggzy_data/ggzy.js
@@ -14,4 +14,4 @@ function b(t) {
         }
 
 console.log(b(Data))
-
+//
--- jiem/gonggzy_data/ggzy.py
+++ jiem/gonggzy_data/ggzy.py
@@ -1,21 +1,24 @@
 import json
 
-import requests
 
 import execjs
 
+import requests
+
 headers = {
     'Accept': 'application/json, text/plain, */*',
-    'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+    'Cache-Control': 'no-cache',
     'Connection': 'keep-alive',
     'Content-Type': 'application/json;charset=UTF-8',
-    'Origin': 'https://ggzyfw.fj.gov.cn',
-    'Referer': 'https://ggzyfw.fj.gov.cn/business/list/',
+    'Origin': 'https://ggzyfw.fujian.gov.cn',
+    'Pragma': 'no-cache',
+    'Referer': 'https://ggzyfw.fujian.gov.cn/business/list/',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
-    'portal-sign': '43cf223bf4c26b8b247b1d523af6d882',
+    'portal-sign': '9cfb12a7140ec300c265b88dd6892cc2',
     'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
@@ -31,17 +34,17 @@ json_data = {
     'GGTYPE': '1',
     'PROTYPE': '',
     'timeType': '6',
-    'BeginTime': '2022-11-21 00:00:00',
-    'EndTime': '2023-05-21 23:59:59',
+    'BeginTime': '2022-11-23 00:00:00',
+    'EndTime': '2023-05-23 23:59:59',
     'createTime': [],
-    'ts': 1684674757813,
+    'ts': 1684839920928,
 }
 
-response = requests.post('https://ggzyfw.fj.gov.cn/FwPortalApi/Trade/TradeInfo', headers=headers, json=json_data)
-print(response.json())
+response = requests.post('https://ggzyfw.fujian.gov.cn/FwPortalApi/Trade/TradeInfo', headers=headers, json=json_data)
 
-ctx = execjs.compile(open("ggzy.js","r").read())
-ctx.call("b",json.loads(response.text).Data)
+
+ctx = execjs.compile(open("ggzy.js", "r").read())
+print(ctx.call("b",response.json()["Data"]))
 
 
 # Note: json_data will not be serialized by requests
