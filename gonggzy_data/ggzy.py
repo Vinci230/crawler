f801b522635772bae137dd16c9582efdea060379
Vinci230 <lienxithy@gmail.com>
2023-05-23 23:18:55 +0800

    hh

--- gonggzy_data/ggzy.py
+++ gonggzy_data/ggzy.py
@@ -1,20 +1,12 @@
 import time
 
-import requests
-
 import execjs
 
+import requests
 
+ctx = execjs.compile(open("ggzy.js", "r").read())
 
-
-date = int(time.time()*1000)
-
-
-
-
-
-
-
+date = round(time.time() * 1000)
 
 json_data = {
     'pageNo': 1,
@@ -26,52 +18,40 @@ json_data = {
     'GGTYPE': '1',
     'PROTYPE': '',
     'timeType': '6',
-    'BeginTime': '2022-11-21 00:00:00',
-    'EndTime': '2023-05-21 23:59:59',
+    'BeginTime': '2022-11-23 00:00:00',
+    'EndTime': '2023-05-23 23:59:59',
     'createTime': [],
     'ts': date,
 }
-ctx = execjs.compile(open("ggzy.js","r",encoding="utf-8").read())
-
-t={
-        "ts": date,
-        "pageNo": 2,
-        "pageSize": 20,
-        "total": 3626,
-        "AREACODE": "",
-        "M_PROJECT_TYPE": "",
-        "KIND": "GCJS",
-        "GGTYPE": "1",
-        "PROTYPE": "",
-        "timeType": "6",
-        "BeginTime": "2022-11-21 00:00:00",
-        "EndTime": "2023-05-21 23:59:59",
-        "createTime": []
-    }
 
 headers = {
     'Accept': 'application/json, text/plain, */*',
-    'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Accept-Language': 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+    'Cache-Control': 'no-cache',
     'Connection': 'keep-alive',
     'Content-Type': 'application/json;charset=UTF-8',
-    'Origin': 'https://ggzyfw.fj.gov.cn',
-    'Referer': 'https://ggzyfw.fj.gov.cn/business/list/',
+    'Origin': 'https://ggzyfw.fujian.gov.cn',
+    'Pragma': 'no-cache',
+    'Referer': 'https://ggzyfw.fujian.gov.cn/business/list/',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-origin',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
-    'portal-sign': ctx.call("d",t),
+    'portal-sign': ctx.call("d",json_data),
     'sec-ch-ua': '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
 }
-print(headers)
-
-response = requests.post('https://ggzyfw.fj.gov.cn/FwPortalApi/Trade/TradeInfo',  headers=headers, json=json_data)
-print(response.json())
 
 
-ctx.call("b",response.json()["Data"])
+
+response = requests.post('https://ggzyfw.fujian.gov.cn/FwPortalApi/Trade/TradeInfo', headers=headers, json=json_data)
+print(headers['portal-sign'])
+
+print(response.text)
+
+
+print(ctx.call("b",response.json()["Data"]))
 
 
 # Note: json_data will not be serialized by requests
