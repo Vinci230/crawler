8d0ab6348204f541ebd3b2e5b6ebeb963a01df04
Vinci230 <lienxithy@gmail.com>
2023-05-31 15:23:48 +0800

    臭嵌套

--- zy/zy.js
+++ zy/zy.js
@@ -1,30 +1,74 @@
-//嵌套
-// setTimeout(function() {
-//     console.log('a');
-//     setTimeout(function(){
-//       console.log('b');
-//         setTimeout(function(){
-//            console.log('c')
-// },3000);
-//
-// },3000);
-//
-//
-//
-// },3000);
-
-//链式回调
-let p = new Promise((resolve, reject) => {
-//做一些异步操作
-setTimeout(() => {
-console.log("a")
-
-
- }, 3000);
+setTimeout(function() {
+    console.log('a');
+    setTimeout(function(){
+        console.log('b');
+        setTimeout(function(){
+            console.log('c');
+        },3000)
+},3000)
+}, 3000);
+//promise 链式回调
+new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        console.log('a');
+        resolve()
+    }, 3000)
+    
+}).then(() =>{   //执行.then的时候，将b生成为一个promise对象
+     b();
 })
-.then(
-(data) => {
-console.log(data);
-return data;})
-//此时输出data为resolve传入的参数
+var b = function(){
+    return new  Promise((resolve,reject)=>{
+        setTimeout(() => {
+            console.log('b');
+            resolve()
+        }, 3000)
+     
+    }).then(() =>{
+         c();
+    })
+}
+var c =  function(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            console.log('c');
+            resolve()
+        }, 3000)
+       
+    })
+    
+}
+//aysnc await异步队列
+let a=(time)=>{
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+};
+var b=(time)=>{
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+};
+var c=(time)=>{
+    return new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+};
 
+
+async function demo()
+{
+    let word=await a(3000);
+    console.log('a');
+    let word1=await b(3000);
+    console.log('b');
+    let word2=await c(3000);
+    console.log('c');
+}
+demo()
